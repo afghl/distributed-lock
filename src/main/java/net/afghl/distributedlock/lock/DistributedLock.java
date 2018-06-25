3c43b06960b056448dd98ff94ed2cc0afeb7de09
afghl <airport.0930@gmail.com>
2018-06-25 19:57:39 +0800

    fix api

---
+++ src/main/java/net/afghl/distributedlock/lock/DistributedLock.java
@@ -0,0 +1,8 @@
+package net.afghl.distributedlock.lock;
+
+import java.util.ArrayList;
+import java.util.concurrent.locks.Lock;
+import java.util.stream.Stream;
+
+public interface DistributedLock extends Lock {
+}
--- src/main/java/net/afghl/distributedlock/lock/LockHolder.java
+++
@@ -1,7 +0,0 @@
-package net.afghl.distributedlock.lock;
-
-import java.io.Closeable;
-
-public interface LockHolder extends Closeable {
-
-}
